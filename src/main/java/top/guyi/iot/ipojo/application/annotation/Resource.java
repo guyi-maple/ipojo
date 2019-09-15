2765333b4c593873c5143b8666c2c31fb8af7202
guyi <love-jia-jun@outlook.com>
2019-09-15 23:49:24 +0800

    update

--- src/main/java/top/guyi/iot/ipojo/application/annotation/Resource.java
+++ src/main/java/top/guyi/iot/ipojo/application/annotation/Resource.java
@@ -9,6 +9,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Resource {
 
-    String value() default "";
+    String name() default "";
+
+    boolean equals() default true;
 
 }
